9ff59358792485ec71e43d21d3a35d63bbb8780b
clarkj99 <clark@stontech.net>
2020-01-14 14:56:25 -0500

    corrected enums

--- app/models/hpi.rb
+++ app/models/hpi.rb
@@ -1,6 +1,6 @@
 class Hpi < ApplicationRecord
   belongs_to :encounter
 
-  enum duration_units = [:days, :weeks, :months]
-  enum aggravating_factors = [:movement, :rest, :intercourse, :bowel_movement, :pressure, :touch, :urination]
+  enum duration_units: [:days, :weeks, :months]
+  enum aggravating_factors: [:movement, :rest, :intercourse, :bowel_movement, :pressure, :touch, :urination]
 end
