55c737551942b152d4fed6ec4d0b7ea0c4107910
clarkj99 <clark@stontech.net>
2020-01-14 14:21:48 -0500

    routes for rosystem

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
       patch "encounter/:id", to: "encounters#update"
       get "hpi/:id", to: "hpis#show"
       patch "hpi/:id", to: "hpis#update"
+      get "rosystem/:id", to: "rosystems#show"
+      patch "rosystem/:id", to: "rosystems#update"
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
