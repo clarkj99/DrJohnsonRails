4336555fc79d1d8a58e7fbf3647f83cd00b5b265
clarkj99 <clark@stontech.net>
2020-01-23 15:02:06 -0500

    removed icd10 search restrictions

--- app/controllers/api/v1/icd10s_controller.rb
+++ app/controllers/api/v1/icd10s_controller.rb
@@ -2,8 +2,9 @@ class Api::V1::Icd10sController < ApplicationController
   before_action :find_icd10, only: [:show]
 
   def index
-    if params[:term].length > 1
-      @icd10s = Icd10.where("(lower(code) like ?) OR ((lower(code) like 'o%'  OR lower(code) like 'm%') AND lower(description) like ?)", "%#{params[:term].downcase}%", "%#{params[:term].downcase}%")
+    if params[:term].length > 2
+      # @icd10s = Icd10.where("(lower(code) like ?) OR ((lower(code) like 'o%'  OR lower(code) like 'm%') AND lower(description) like ?)", "%#{params[:term].downcase}%", "%#{params[:term].downcase}%")
+      @icd10s = Icd10.where("(lower(code) like ?) OR (lower(description) like ?)", "%#{params[:term].downcase}%", "%#{params[:term].downcase}%")
     else
       # @icd10s = Icd10.where("code like 'O%'  OR code like 'M%'").limit(10)
       @icd10s = []
